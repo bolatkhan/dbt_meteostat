211b8ddbde4280a34c828f0783831025238431ec
bolatkhan <askhat.bolatkhan@gmail.com>
2025-03-12 11:43:46 +0100

    asd

--- models/staging/staging_weather_daily.sql
+++ models/staging/staging_weather_daily.sql
@@ -4,6 +4,7 @@ WITH daily_raw AS (
             station_id,
             JSON_ARRAY_ELEMENTS(extracted_data -> 'data') AS json_data
     FROM {{source('weather_data', 'weather_daily_raw')}}
+ --           FROM weather_daily_raw
 ),
 daily_flattened AS (
     SELECT  airport_code,
@@ -22,4 +23,39 @@ daily_flattened AS (
     FROM daily_raw
 )
 SELECT * 
-FROM daily_flattened
+FROM daily_flattened;
+
+
+
+WITH hourly_data AS (
+  SELECT * 
+-- FROM {{ref('staging_weather_hourly')}}
+  FROM staging_weather_hourly
+),
+add_features AS (
+  SELECT *
+    , timestamp::DATE AS date -- only date (year-month-day) as DATE data type
+    , timestamp::TIME AS time -- only time (hours:minutes:seconds) as TIME data type
+    , TO_CHAR(timestamp,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
+    , TO_CHAR(timestamp, 'FMmonth') AS month_name -- month name as a TEXT
+    , TO_CHAR(timestamp, 'FMday') AS weekday    -- weekday name as TEXT      
+    , DATE_PART('day', timestamp) AS date_day
+    , DATE_PART('month', timestamp) AS date_month
+    , DATE_PART('year', timestamp) AS date_year
+    , DATE_PART('week', timestamp) AS cw
+  FROM hourly_data
+),
+add_more_features AS (
+  SELECT *
+    ,(CASE 
+        WHEN time BETWEEN '00:00:00' AND '05:59:00' THEN 'night'
+        WHEN time BETWEEN '06:00:00' AND '18:00:00' THEN 'day'
+        WHEN time BETWEEN '18:00:00' AND '23:59:00' THEN 'evening'
+    END) AS day_part
+  FROM add_features
+)
+SELECT *
+FROM add_more_features;
+
+
+SELECT * from staging_airports;
